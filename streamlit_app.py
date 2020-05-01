9e72348019ac783947f975dc22bc9164b811373f
diego <dm.matukb@alum.up.edu.pe>
2020-05-01 15:57:49 -0500

    added lat long coordinates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
 import streamlit as st
+import webbrowser
 
 import pandas as pd
 import numpy as np
 from objects.user import User
+from objects.dataframe_class import DataFrame_c
+import _pickle as cPickle
 
 st.sidebar.header('Rentabilidad en Bienes Raices')
 seleccion = st.sidebar.radio('Tipos de servicio',('Prediccion de alquiler','Inversiones mas rentables'))
@@ -37,23 +40,93 @@ def prediccion_alq():
         antiguedad = st.number_input('Antigüedad (en años) del edificio/casa',value = 0,min_value = 0,step = 1)
         parque = st.radio('¿Tiene un parque cerca? Puede ser interno o no',('Si','No'))
 
-        tipo = st.radio('Casa o Departamento',('Casa','Departamento'))
+        if parque == 'Si':
+            parque = 1
+        else:
+            parque = 0
 
+        tipo = st.radio('Casa o Departamento',('Casa','Departamento'))
+        cant_pisos = st.selectbox('¿Cuántos pisos tiene?',[1,2,3,4,5])
         if tipo == 'Casa':
-            pass
+            piso = 1
 
         elif tipo == 'Departamento':
             piso = st.slider('Que piso se encuentra',1,25)
 
 
+    prediccion = User().compute(m2 = m2, m2_techados = m2_techados,parking = parqueo,dorms = dormitorios,
+                                antiguedad = antiguedad,park_near = parque,floor = piso, mantenimiento = 0,
+                                cant_pisos = cant_pisos,type = tipo, distrito = distrito)
+
+    upper_lim = round(np.exp(prediccion[0] + 0.2774),0)
+    lower_lim = round(np.exp(prediccion[0]))
+
+    st.markdown(f'''<h1 style='text-align: center; color: red;'>Lo podrias alquilar entre:
+                    S/ {int(lower_lim)} y {int(upper_lim)}</h1>''',unsafe_allow_html=True)
 
 
 def rentabilidad():
-    pass
+    df_class = DataFrame_c()
+    df = df_class.origin_df('outputs/houses/')
+    df['Distrito'] = df['Ubicacion'].apply(df_class.distritos_apply)
+    df['Antiguedad del edificio'] = df['Antiguedad del edificio'].apply(df_class.correc_antiquy)
+    df['Parque cercano'] = df['Parque cercano'].apply(df_class.correc_near)
+    df['Mantenimiento'] = df['Mantenimiento'].apply(df_class.correc_mant)
+    df['Parqueo'] = df['Parqueo'].apply(df_class.correc_parking)
+    df['Venta'] = df['Venta'].apply(df_class.correc_alquiler)
+    df['Metros cuadrados'] = df['Metros cuadrados'].apply(df_class.correc_m2)
+    df['Metros cuadrados techados'] = df['Metros cuadrados techados'].apply(df_class.correc_m2)
+    df['Dormitorios'] = df['Dormitorios'].apply(df_class.correc_dorms)
+    df['Piso en el que se encuentra'] = df['Piso en el que se encuentra'].apply(df_class.correc_floor)
+
+    del df['Piso en el que se encuentra']
+    df[['Metros cuadrados','Metros cuadrados techados','Dormitorios']] = df[['Metros cuadrados','Metros cuadrados techados','Dormitorios']].replace('No hay informacion',np.nan)
+    df['Metros cuadrados'] = pd.to_numeric(df['Metros cuadrados'])
+    df['Metros cuadrados techados'] = pd.to_numeric(df['Metros cuadrados techados'])
+    df['Dormitorios'] = pd.to_numeric(df['Dormitorios'])
+    df['Parqueo'] = pd.to_numeric(df['Parqueo'])
+
+
+    df = df[df['Venta'] > 45000]
+    st.dataframe(df)
+
+
+
+
 
 
 if __name__ == '__main__':
     main()
+
+
+
+
+#
+# '''  LINKS IN BUTTONS
+#     url = 'https://www.streamlit.io/'
+#
+#     if st.button('Open browser'):
+#         webbrowser.open_new_tab(url)
+#    '''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # #Ttile
 # st.title('Titulo')
