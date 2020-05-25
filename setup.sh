17e70ed42da31934525a4bfc3024bf61749796e5
diego <dm.matukb@alum.up.edu.pe>
2020-05-25 09:27:36 -0500

    setup

--- setup.sh
+++ setup.sh
@@ -1,15 +1,8 @@
 mkdir -p ~/.streamlit/
 
 
-echo “\
-[general]\n\
-email = \”diegomatukb@gmail.com\”\n\
-“ > ~/.streamlit/credentials.toml
-
-
-echo “\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-“ > ~/.streamlit/config.toml
+echo "[server]
+headless = true
+port = $PORT
+enableCORS = false
+" > ~/.streamlit/config.toml
